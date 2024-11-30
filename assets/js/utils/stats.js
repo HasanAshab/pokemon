8eeee50443d99903c5eaa352dbd0343e46b319fa
HasanAshab <samertask@gmail.com>
2024-11-30 11:35:19 +0600

    backup

--- assets/js/utils/stats.js
+++ assets/js/utils/stats.js
@@ -1,4 +1,10 @@
 export function applyStatChanges(attacker, target, move) {
+    const statChanged = move.meta.stat_chance === 0 
+      || move.meta.stat_chance === 100
+      || Math.random() < (move.meta.stat_chance / 100)
+    
+    if (!statChanged) return
+
     // Apply changes to target's stat stages
     for (const [stat, change] of Object.entries(move.stat_changes.target)) {
         target.state.applyStatChange(stat, change)
