59332b451cbaf47b4717b393a0c54e1fa50491a9
Hasan Ashab <110120731+HasanAshab@users.noreply.github.com>
2024-12-21 09:51:15 +0600

    Update enemy.js

--- assets/js/enemy.js
+++ assets/js/enemy.js
@@ -1,52 +1,66 @@
-import db from "./utils/db.js";
-import { Pokemon } from "./utils/models.js";
+import { Pokemon,Move } from "./utils/models.js";
 import { getParam } from "./utils/helpers.js"
-
-async function loadPokemonsDatalist() {
-    const pokemons = await db.pokemons.all();
-    const dataList = document.getElementById("enemy-data-list");
-    dataList.innerHTML = pokemons.map(pokemon => `<option value="${pokemon}">${pokemon}(${pokemon.type})</option>`).join("");
-
-}
-
-async function loadMovesDatalist() {
-    const moves = await db.moves.all();
-    const moveDataList = document.getElementById("moves-data-list");
-    moveDataList.innerHTML = moves.map(move => `<option value="${move}">${move}</option>`).join("");
-}
+import { loadPokemonsDatalist, loadNaturesDataList, loadMovesDatalist } from "./utils/dom.js";
 
 
 
 window.onload = () => {
-    loadPokemonsDatalist()
-    loadMovesDatalist()
+    loadPokemonsDatalist("enemy-data-list")
+    loadNaturesDataList("natures-data-list")
+    loadMovesDatalist("moves-data-list")
 }
 
-const enemy = document.getElementById("enemy");
-const levelInp = document.getElementById("level-inp");
-const retreatInp = document.getElementById("retreat-inp");
-const natureInp = document.getElementById("nature-inp");
-const enemyStats = document.getElementById("enemy-stats");
+const enemy = document.querySelector(".enemy");
+const levelInp = document.querySelector(".level-inp");
+const retreatInp = document.querySelector(".retreat-inp");
+const natureInp = document.querySelector(".nature-inp");
+const tokenInp = document.querySelector(".token-inp");
+const enemyStats = document.querySelector(".enemy-stats");
 
-globalThis.showStats = async function() {
-    const level = levelInp.value;
+function makePokemon() {
+    const level = parseInt(levelInp.value);
+    const retreat = parseInt(retreatInp.value);
     const nature = natureInp.value;
-    const enemyPokemon = await Pokemon.make(enemy.value, { xp: level * 100, nature });
-    enemyStats.innerHTML = JSON.stringify(enemyPokemon.data.stats,  null, 2);
+    const tokens = tokenInp.value ? JSON.parse(tokenInp.value) : {};
+    const moves = [
+        document.querySelector(".move-input-1").value,
+        document.querySelector(".move-input-2").value,
+        document.querySelector(".move-input-3").value,
+        document.querySelector(".move-input-4").value,
+        document.querySelector(".move-input-5").value,
+    ].filter(Boolean).map(id => ({
+        id,
+        isSelected: true
+    }))
+    
+    return new Pokemon(enemy.value, {
+        xp: level * 100,
+        nature,
+        retreat,
+        moves,
+        token_used: tokens
+    });
+}
+globalThis.showStats = async function() {
+    const enemyPokemon = makePokemon()
+    enemyStats.innerHTML = JSON.stringify(enemyPokemon.stats,  null, 2);
 }
 
+globalThis.showMoveDetails = function({currentTarget}){
+  const move = new Move(currentTarget.value)
+  if (move){
+    const moveDetails = document.getElementById("move-details")
+    moveDetails.querySelector(".move-name").textContent = move.name
+    moveDetails.querySelector(".desc").textContent = move.description()
+  
+ }
+}
 
 globalThis.startBattle = function() {
-    const level = levelInp.value;
-    const retreat = retreatInp.value;
-    const nature = natureInp.value;
-    const moves = [
-        document.getElementById("move-input-1").value,
-        document.getElementById("move-input-2").value,
-        document.getElementById("move-input-3").value,
-        document.getElementById("move-input-4").value,
-        document.getElementById("move-input-5").value,
-    ]
+    const enemyBase64 = makePokemon().toBase64();
+    window.location = `battle.html?you=${getParam("name")}&enemy=${enemyBase64}`;
+}
 
-    window.location = `battle.html?you=${getParam("name")}&enemy=${enemy.value}&xp=${level * 100}&retreat=${retreat}&nature=${nature}&moves=${moves.join(',')}`;
+globalThis.randomBattle = function() {
+    window.location = `battle.html?you=${getParam("name")}&enemy=eyJpZCI6ImJ1bGJhc2F1ciIsIm1ldGEiOnsieHAiOjUwMCwibmF0dXJlIjoiY2FsbSIsInJldHJlYXQiOjIsIm1vdmVzIjpbeyJpZCI6ImFjaWQiLCJpc1NlbGVjdGVkIjp0cnVlfSx7ImlkIjoiYWJzb3JiIiwiaXNTZWxlY3RlZCI6dHJ1ZX1dLCJ0b2tlbl91c2VkIjp7fX19`
 }
