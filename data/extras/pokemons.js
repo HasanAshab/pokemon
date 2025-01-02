c7f575f2e09dfb0c5646a5c6a31ac64fdac86dde
HasanAshab <samertask@gmail.com>
2025-01-02 10:01:58 +0600

    backup

--- data/extras/pokemons.js
+++ data/extras/pokemons.js
@@ -1,3 +1,18 @@
 export default {
-    //
+    charizardmegaz: {
+      num: 6,
+      name: "Charizard-Mega-Z",
+      baseSpecies: "Charizard",
+      forme: "Mega-Z",
+      types: ["Fire", "Poison", "Dark", "Dragon"],
+      genderRatio: { M: 0.875, F: 0.125 },
+      baseStats: { hp: 78, atk: 150, def: 171, spa: 120, spd: 45, spe: 70 },
+      abilities: { 0: "Tough Claws" },
+      heightm: 1.7,
+      weightkg: 120,
+      color: "Black",
+      eggGroups: ["Monster", "Dragon"],
+      requiredItem: "Charizardite Z"
+    },
+
 }
