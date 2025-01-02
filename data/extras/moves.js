127c07f1bf1be0d8e2f972c002a54855a6bc08e1
HasanAshab <samertask@gmail.com>
2025-01-02 18:40:42 +0600

    b

--- data/extras/moves.js
+++ data/extras/moves.js
@@ -1,3 +1,5 @@
+//import { calculateDTManCount } from "../processor/"
+
 export default {
     staythere: {
       num: 100001,
@@ -28,5 +30,22 @@ export default {
       type: "Normal",
       isOffensive: false,
       retreat: 0.25
+    },
+    doubleteam: {
+      num: 104,
+      accuracy: true,
+      basePower: 0,
+      category: "Status",
+      name: "Double Team",
+      pp: 15,
+      priority: 0,
+      flags: { snatch: 1, metronome: 1 },
+      secondary: null,
+      target: "self",
+      type: "Normal",
+      contestType: "Cool",
+      onHit(pokemon) {
+        pokemon.state.manCount = calculateDTManCount(pokemon)
+      }
     },
 }
