f9f5fdb58d46203c5ddad5247b0d122518f00489
HasanAshab <samertask@gmail.com>
2025-01-11 09:59:10 +0600

    backup

---
+++ data/default/types.js
@@ -0,0 +1,161 @@
+export default {
+    "Normal": {
+        "Rock": "half",
+        "Steel": "half",
+        "Ghost": "immune"
+    },
+    "Fighting": {
+        "Normal": "double",
+        "Rock": "double",
+        "Steel": "double",
+        "Ice": "double",
+        "Dark": "double",
+        "Flying": "half",
+        "Poison": "half",
+        "Bug": "half",
+        "Psychic": "half",
+        "Fairy": "half",
+        "Ghost": "immune"
+    },
+    "Flying": {
+        "Fighting": "double",
+        "Bug": "double",
+        "Grass": "double",
+        "Rock": "half",
+        "Steel": "half",
+        "Electric": "half"
+    },
+    "Poison": {
+        "Grass": "double",
+        "Fairy": "double",
+        "Poison": "half",
+        "Ground": "half",
+        "Rock": "half",
+        "Ghost": "half",
+        "Steel": "immune"
+    },
+    "Ground": {
+        "Poison": "double",
+        "Rock": "double",
+        "Steel": "double",
+        "Fire": "double",
+        "Electric": "double",
+        "Bug": "half",
+        "Grass": "half",
+        "Flying": "immune"
+    },
+    "Rock": {
+        "Flying": "double",
+        "Bug": "double",
+        "Fire": "double",
+        "Ice": "double",
+        "Fighting": "half",
+        "Ground": "half",
+        "Steel": "half"
+    },
+    "Bug": {
+        "Grass": "double",
+        "Psychic": "double",
+        "Dark": "double",
+        "Fighting": "half",
+        "Flying": "half",
+        "Poison": "half",
+        "Ghost": "half",
+        "Steel": "half",
+        "Fire": "half",
+        "Fairy": "half"
+    },
+    "Ghost": {
+        "Ghost": "double",
+        "Psychic": "double",
+        "Dark": "half",
+        "Normal": "immune"
+    },
+    "Steel": {
+        "Rock": "double",
+        "Ice": "double",
+        "Fairy": "double",
+        "Steel": "half",
+        "Fire": "half",
+        "Water": "half",
+        "Electric": "half"
+    },
+    "Fire": {
+        "Bug": "double",
+        "Steel": "double",
+        "Grass": "double",
+        "Ice": "double",
+        "Rock": "half",
+        "Fire": "half",
+        "Water": "half",
+        "Dragon": "half"
+    },
+    "Water": {
+        "Ground": "double",
+        "Rock": "double",
+        "Fire": "double",
+        "Water": "half",
+        "Grass": "half",
+        "Dragon": "half"
+    },
+    "Grass": {
+        "Ground": "double",
+        "Rock": "double",
+        "Water": "double",
+        "Flying": "half",
+        "Poison": "half",
+        "Bug": "half",
+        "Steel": "half",
+        "Fire": "half",
+        "Grass": "half",
+        "Dragon": "half"
+    },
+    "Electric": {
+        "Flying": "double",
+        "Water": "double",
+        "Grass": "half",
+        "Electric": "half",
+        "Dragon": "half",
+        "Ground": "immune"
+    },
+    "Psychic": {
+        "Fighting": "double",
+        "Poison": "double",
+        "Steel": "half",
+        "Psychic": "half",
+        "Dark": "immune"
+    },
+    "Ice": {
+        "Flying": "double",
+        "Ground": "double",
+        "Grass": "double",
+        "Dragon": "double",
+        "Steel": "half",
+        "Fire": "half",
+        "Water": "half",
+        "Ice": "half"
+    },
+    "Dragon": {
+        "Dragon": "double",
+        "Steel": "half",
+        "Fairy": "immune"
+    },
+    "Dark": {
+        "Ghost": "double",
+        "Psychic": "double",
+        "Fighting": "half",
+        "Dark": "half",
+        "Fairy": "half"
+    },
+    "Fairy": {
+        "Fighting": "double",
+        "Dragon": "double",
+        "Dark": "double",
+        "Poison": "half",
+        "Steel": "half",
+        "Fire": "half"
+    },
+    "Stellar": {},
+    "Unknown": {},
+    "Shadow": {}
+}
