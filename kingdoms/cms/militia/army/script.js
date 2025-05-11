23f94cb6bef96203026c220e9018ba42a7c44e0c
Hasan Ashab <samertask@gmail.com>
2025-05-11 16:59:24 +0600

    backup

---
+++ kingdoms/cms/militia/army/script.js
@@ -0,0 +1,17 @@
+
+
+const urlParams = new URLSearchParams(window.location.search);
+const name = urlParams.get('name');
+
+const kingdomName = document.getElementById('kingdomName');
+kingdomName.textContent = name || 'Unknown Kingdom';
+
+
+document.querySelectorAll('.info-card').forEach(card => {
+  card.addEventListener('click', () => {
+    const target = card.getAttribute('data-target');
+    if (!name || !target) return;
+    const encoded = encodeURIComponent(name);
+    window.location.href = `/kingdoms/cms/militia/${target}/?name=${encoded}`;
+  });
+});
