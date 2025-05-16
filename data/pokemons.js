1b4a8297b5745ec043e56d3683c7e271e186d1f8
Hasan Ashab <samertask@gmail.com>
2025-05-16 14:50:19 +0600

    backup

--- data/pokemons.js
+++ data/pokemons.js
@@ -2,148 +2,166 @@ export default {
   student: {
     num: 1,
     name: "Student",
-    types: ["Training"],
-    genderRatio: {
-      M: 0.5,
-      F: 0.5
-    },
-    baseStats: {
-      hp: 30,
-      atk: 20,
-      def: 20,
-      spa: 15,
-      spd: 15,
-      spe: 25
-    },
+    types: [],
+    baseStats: { hp: 30, atk: 20, def: 20, spa: 15, spd: 15, spe: 25 },
     abilities: {},
     heightm: 1.4,
-    weightkg: 40,
+    weightkg: 45,
     color: "Blue",
-    evos: ["Genin"],
-    eggGroups: ["Human"]
+    evos: ["Rookie"]
+  },
+  rookie: {
+    num: 2,
+    name: "Rookie",
+    types: [],
+    baseStats: { hp: 36, atk: 24, def: 22, spa: 18, spd: 19, spe: 32 },
+    abilities: {},
+    heightm: 1.45,
+    weightkg: 47,
+    color: "Blue",
+    evos: ["Genin"]
   },
   genin: {
-    num: 2,
+    num: 3,
     name: "Genin",
-    types: ["Shinobi"],
-    genderRatio: {
-      M: 0.5,
-      F: 0.5
-    },
-    baseStats: {
-      hp: 40,
-      atk: 35,
-      def: 30,
-      spa: 30,
-      spd: 30,
-      spe: 35
-    },
+    types: [],
+    baseStats: { hp: 40, atk: 32, def: 28, spa: 22, spd: 24, spe: 38 },
     abilities: {},
     heightm: 1.5,
-    weightkg: 45,
-    color: "Gray",
-    prevo: "Academy Student",
-    evoLevel: 10,
-    evos: ["Chunin"],
-    eggGroups: ["Human"]
+    weightkg: 50,
+    color: "Green",
+    evos: ["Chunin"]
   },
   chunin: {
-    num: 3,
+    num: 4,
     name: "Chunin",
-    types: ["Shinobi"],
-    genderRatio: {
-      M: 0.5,
-      F: 0.5
-    },
-    baseStats: {
-      hp: 55,
-      atk: 50,
-      def: 45,
-      spa: 40,
-      spd: 45,
-      spe: 50
-    },
+    types: [],
+    baseStats: { hp: 52, atk: 35, def: 33, spa: 28, spd: 30, spe: 41 },
     abilities: {},
-    heightm: 1.6,
-    weightkg: 50,
-    color: "Brown",
-    prevo: "Genin",
-    evoLevel: 25,
-    evos: ["Jonin"],
-    eggGroups: ["Human"]
+    heightm: 1.55,
+    weightkg: 55,
+    color: "Green",
+    evos: ["Jonin"]
   },
   jonin: {
-    num: 4,
+    num: 5,
     name: "Jonin",
-    types: ["Elite"],
-    genderRatio: {
-      M: 0.5,
-      F: 0.5
-    },
-    baseStats: {
-      hp: 75,
-      atk: 70,
-      def: 65,
-      spa: 60,
-      spd: 65,
-      spe: 70
-    },
+    types: [],
+    baseStats: { hp: 60, atk: 45, def: 40, spa: 36, spd: 38, spe: 53 },
     abilities: {},
-    heightm: 1.7,
-    weightkg: 55,
+    heightm: 1.6,
+    weightkg: 60,
     color: "Black",
-    prevo: "Chunin",
-    evoLevel: 40,
-    evos: ["Anbu",
-      "Kage"],
-    eggGroups: ["Human"]
+    evos: ["Rogue", "Anbu"]
+  },
+  rogue: {
+    num: 6,
+    name: "Rogue",
+    types: [],
+    baseStats: { hp: 62, atk: 58, def: 30, spa: 48, spd: 31, spe: 66 },
+    abilities: {},
+    heightm: 1.65,
+    weightkg: 58,
+    color: "Red",
+    evos: ["Koga"]
   },
   anbu: {
-    num: 5,
+    num: 7,
     name: "Anbu",
-    types: ["Stealth"],
-    genderRatio: {
-      M: 0.5,
-      F: 0.5
-    },
-    baseStats: {
-      hp: 80,
-      atk: 85,
-      def: 70,
-      spa: 60,
-      spd: 70,
-      spe: 85
-    },
+    types: [],
+    baseStats: { hp: 64, atk: 50, def: 50, spa: 42, spd: 42, spe: 68 },
+    abilities: {},
+    heightm: 1.65,
+    weightkg: 62,
+    color: "Black",
+    evos: ["Koga"]
+  },
+  koga: {
+    num: 8,
+    name: "Koga",
+    types: [],
+    baseStats: { hp: 70, atk: 65, def: 55, spa: 56, spd: 49, spe: 72 },
+    abilities: {},
+    heightm: 1.7,
+    weightkg: 65,
+    color: "Purple",
+    evos: ["Sannin"]
+  },
+  sannin: {
+    num: 9,
+    name: "Sannin",
+    types: [],
+    baseStats: { hp: 82, atk: 68, def: 61, spa: 69, spd: 60, spe: 76 },
     abilities: {},
     heightm: 1.75,
-    weightkg: 58,
-    color: "Silver",
-    prevo: "Jonin",
-    evoLevel: 50,
-    eggGroups: ["Human"]
+    weightkg: 68,
+    color: "Gold",
+    evos: ["Sage"]
   },
-  kage: {
-    num: 6,
-    name: "Kage",
-    types: ["Leader"],
-    genderRatio: {
-      M: 0.5,
-      F: 0.5
-    },
-    baseStats: {
-      hp: 95,
-      atk: 90,
-      def: 85,
-      spa: 80,
-      spd: 85,
-      spe: 75
-    },
+  sage: {
+    num: 10,
+    name: "Sage",
+    types: [],
+    baseStats: { hp: 90, atk: 63, def: 67, spa: 88, spd: 70, spe: 74 },
     abilities: {},
     heightm: 1.8,
-    weightkg: 60,
+    weightkg: 70,
     color: "Gold",
-    prevo: "Jonin",
-    evoLevel: 60,
-    eggGroups: ["Human"]
+    evos: ["Shadow"]
+  },
+  shadow: {
+    num: 11,
+    name: "Shadow",
+    types: [],
+    baseStats: { hp: 94, atk: 70, def: 65, spa: 93, spd: 65, spe: 88 },
+    abilities: {},
+    heightm: 1.85,
+    weightkg: 75,
+    color: "Dark Gray",
+    evos: ["Kage"]
+  },
+  kage: {
+    num: 12,
+    name: "Kage",
+    types: [],
+    baseStats: { hp: 106, atk: 80, def: 83, spa: 90, spd: 79, spe: 91 },
+    abilities: {},
+    heightm: 1.9,
+    weightkg: 80,
+    color: "Crimson",
+    evos: ["Hokage"]
+  },
+  hokage: {
+    num: 13,
+    name: "Hokage",
+    types: [],
+    baseStats: { hp: 112, atk: 85, def: 89, spa: 104, spd: 88, spe: 92 },
+    abilities: {},
+    heightm: 2.0,
+    weightkg: 85,
+    color: "Crimson",
+    evos: ["Phantom"]
+  },
+  phantom: {
+    num: 14,
+    name: "Phantom",
+    types: [],
+    baseStats: { hp: 115, atk: 98, def: 83, spa: 112, spd: 90, spe: 107 },
+    abilities: {},
+    heightm: 2.1,
+    weightkg: 90,
+    color: "Silver",
+    evos: ["Otsutsuki"]
+  },
+  otsutsuki: {
+    num: 15,
+    name: "Otsutsuki",
+    types: [],
+    baseStats: { hp: 125, atk: 100, def: 95, spa: 130, spd: 102, spe: 112 },
+    abilities: {},
+    heightm: 2.2,
+    weightkg: 95,
+    color: "White",
+    evos: []
   }
 }
