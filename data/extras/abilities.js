f9f48a8c0f4f3ad044184623e51144cf3bf38c3b
Your Name <you@example.com>
2025-05-21 07:51:08 +0600

    backup

--- data/extras/abilities.js
+++ data/extras/abilities.js
@@ -1,10 +1,24 @@
 export default {
-  sharingan: {
+  sharinganI: {
     onStart(pokemon) {
       pokemon.state.stats._statChanges.accuracy = 2
     },
     onTryAddVolatile(status, pokemon) {
       if (status.id === "confusion") return null
     }
-  }
+  },
+
+  sharinganII: {
+    onTryBoost(boost, target, source, effect) {
+      if (source && target === source) return
+      if (boost.accuracy && boost.accuracy < 0) {
+        delete boost.accuracy
+      }
+    },
+
+    onTryAddVolatile(status, pokemon) {
+      if (status.id === "confusion") return null
+    }
+  },
+
 }
