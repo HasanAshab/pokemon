a340dd4fecff8a69399a15d0c7fe7d4731d619df
Your Name <you@example.com>
2025-05-22 06:49:21 +0600

    backup

--- data/extras/abilities.js
+++ data/extras/abilities.js
@@ -22,10 +22,17 @@ export default {
 
   sharingan3: {
     onTurn(pokemon) {
+      const AUTO_DODGE_CD = 3
+
+      if (pokemon.state._data.autoDodgeCountDown === undefined) {
+        pokemon.state._data.autoDodgeCountDown = AUTO_DODGE_CD
+      }
+
       pokemon.state._data.autoDodgeCountDown--
-      if (pokemon.state._data.autoDodgeCountDown > 0)
-      pokemon.state._data.autoDodgeCountDown = 3
-      pokemon.state.flags.autoDodge = 1
+      if (pokemon.state._data.autoDodgeCountDown > 0) {
+        pokemon.state._data.autoDodgeCountDown = AUTO_DODGE_CD
+        pokemon.state.flags.autoDodge = 1
+      }
     },
     onTryBoost(boost, target, source, effect) {
       if (source && target === source) return
