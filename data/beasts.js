3ec197e7b60e565a5a12700e58f95093da3a5187
Your Name <you@example.com>
2025-07-07 12:32:11 +0600

    backup

---
+++ data/beasts.js
@@ -0,0 +1,13 @@
+export default {
+  kurama: {
+    num: 1,
+    name: "Student",
+    types: [],
+    baseStats: { hp: 30, atk: 20, def: 20, spa: 15, spd: 15, spe: 25 },
+    abilities: {},
+    heightm: 1.4,
+    weightkg: 45,
+    color: "Blue",
+    evos: ["Rookie"]
+  },
+}
