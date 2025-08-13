494ebdbcbabf5cded58b3356d881a4c22e97f519
Hasan Ashab <samertask@gmail.com>
2025-08-13 11:11:32 +0600

    backup

--- data/humans.js
+++ data/humans.js
@@ -35,7 +35,7 @@ export default {
   chunin: {
     num: 4,
     name: "Chunin",
-    types: ["Normal", "Rock", "Fighting"],
+    types: ["Normal", "Rock", "Steel"],
     baseStats: { hp: 52, atk: 35, def: 33, spa: 28, spd: 30, spe: 41 },
     abilities: {},
     heightm: 1.55,
@@ -46,7 +46,7 @@ export default {
   jonin: {
     num: 5,
     name: "Jonin",
-    types: ["Normal", "Rock", "Fighting", "Psychic"],
+    types: ["Normal", "Rock", "Steel"],
     baseStats: { hp: 60, atk: 45, def: 40, spa: 36, spd: 38, spe: 53 },
     abilities: {},
     heightm: 1.6,
@@ -57,7 +57,7 @@ export default {
   rogue: {
     num: 6,
     name: "Rogue",
-    types: ["Normal", "Rock", "Fighting", "Psychic", "Steel"],
+    types: ["Normal", "Rock", "Steel"],
     baseStats: { hp: 62, atk: 58, def: 30, spa: 48, spd: 31, spe: 66 },
     abilities: {},
     heightm: 1.65,
@@ -68,7 +68,7 @@ export default {
   anbu: {
     num: 7,
     name: "Anbu",
-    types: ["Normal", "Rock", "Fighting", "Psychic", "Steel", "Poison"],
+    types: ["Normal", "Rock", "Steel"],
     baseStats: { hp: 64, atk: 50, def: 50, spa: 42, spd: 42, spe: 68 },
     abilities: {},
     heightm: 1.65,
@@ -79,7 +79,7 @@ export default {
   koga: {
     num: 8,
     name: "Koga",
-    types: ["Normal", "Rock", "Fighting", "Psychic", "Steel", "Poison"],
+    types: ["Normal", "Rock", "Steel"],
     baseStats: { hp: 70, atk: 65, def: 55, spa: 56, spd: 49, spe: 72 },
     abilities: {},
     heightm: 1.7,
@@ -90,7 +90,7 @@ export default {
   sannin: {
     num: 9,
     name: "Sannin",
-    types: ["Normal", "Rock", "Fighting", "Psychic", "Steel", "Poison", "Flying"],
+    types: ["Normal", "Rock", "Steel"],
     baseStats: { hp: 82, atk: 68, def: 61, spa: 69, spd: 60, spe: 76 },
     abilities: {},
     heightm: 1.75,
@@ -101,7 +101,7 @@ export default {
   sage: {
     num: 10,
     name: "Sage",
-    types: ["Normal", "Rock", "Fighting", "Psychic", "Steel", "Poison", "Flying"],
+    types: ["Normal", "Rock", "Steel"],
     baseStats: { hp: 90, atk: 63, def: 67, spa: 88, spd: 70, spe: 74 },
     abilities: {},
     heightm: 1.8,
@@ -112,7 +112,7 @@ export default {
   shadow: {
     num: 11,
     name: "Shadow",
-    types: ["Normal", "Rock", "Fighting", "Psychic", "Steel", "Poison", "Flying", "Dark"],
+    types: ["Normal", "Rock", "Steel"],
     baseStats: { hp: 94, atk: 70, def: 65, spa: 93, spd: 65, spe: 88 },
     abilities: {},
     heightm: 1.85,
@@ -123,7 +123,7 @@ export default {
   kage: {
     num: 12,
     name: "Kage",
-    types: ["Normal", "Rock", "Fighting", "Psychic", "Steel", "Poison", "Flying", "Dark"],
+    types: ["Normal", "Rock", "Steel"],
     baseStats: { hp: 106, atk: 80, def: 83, spa: 90, spd: 79, spe: 91 },
     abilities: {},
     heightm: 1.9,
@@ -134,7 +134,7 @@ export default {
   hokage: {
     num: 13,
     name: "Hokage",
-    types: ["Normal", "Rock", "Fighting", "Psychic", "Steel", "Poison", "Flying", "Dark"],
+    types: ["Normal", "Rock", "Steel"],
     baseStats: { hp: 112, atk: 85, def: 89, spa: 104, spd: 88, spe: 92 },
     abilities: {},
     heightm: 2.0,
@@ -145,7 +145,7 @@ export default {
   phantom: {
     num: 14,
     name: "Phantom",
-    types: ["Normal", "Rock", "Fighting", "Psychic", "Steel", "Poison", "Flying", "Dark", "Ghost"],
+    types: ["Normal", "Rock", "Steel"],
     baseStats: { hp: 115, atk: 98, def: 83, spa: 112, spd: 90, spe: 107 },
     abilities: {},
     heightm: 2.1,
@@ -157,7 +157,7 @@ export default {
     num: 15,
     name: "Otsutsuki",
     types: ["Normal", "Rock", "Fighting", "Psychic", "Steel", "Poison", "Flying", "Dark", "Ghost", "Fairy"],
-    baseStats: { hp: 125, atk: 100, def: 95, spa: 130, spd: 102, spe: 112 },
+    types: ["Normal", "Rock", "Steel"],
     abilities: {},
     heightm: 2.2,
     weightkg: 95,
