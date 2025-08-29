31871dc094226d8548a814101caae8d50ada3546
Hasan Ashab <samertask@gmail.com>
2025-08-30 03:40:51 +0600

    backup

--- assets/js/index.js
+++ assets/js/index.js
@@ -254,117 +254,117 @@ document.body.onload = loadAll;
 
 import { startBattle, startUserBattle } from "./utils/dom.js";
 
-startBattle(null,[
-{
-  "nature": "none",
-  "id": "rookie",
-  "name": "Leader",
-  "xp": 1100,
-  "retreat": 4,
-  "types": [
-    "Normal"
-  ],
-  "abilities": [],
-  "items": [],
-  "moves": [
-    {
-      "id": "kunai",
-      "isSelected": true,
-      "isDefault": true
-    },
-    {
-      "id": "ninjastar",
-      "isSelected": true,
-      "isDefault": true
-    },
-    {
-      "id": "poisongas",
-      "isSelected": true,
-      "isDefault": true
-    },
-    {
-      "id": "fakeout",
-      "isSelected": true,
-      "isDefault": true
-    },
-    {
-      "id": "echoedvoice",
-      "isSelected": true,
-      "isDefault": true
-    },
-    {
-      "id": "faketears"
-    }
-  ],
-  "mega": {
-    "moves": [],
-    "suffix": "mega"
-  },
-  "stats": {},
-  "token_used": {
-    "hp": 0,
-    "spe": 0,
-    "atk": 0,
-    "def": 0,
-    "spa": 0,
-    "spd": 0
-  }
-},  {
-    "id": "student",
-    "name": "E1(tai)",
-    "xp": 1100,
-    "nature": "tai",
-    "retreat": 3,
-    "types": [],
-    "abilities": [],
-    "items": [],
-    "moves": [
-      {
-        "id": "poisonfang",
-        "isSelected": true
-      }
-    ],
-    "mega": {
-      "moves": [],
-      "suffix": "mega"
-    },
-    "stats": {},
-    "token_used": {
-      "hp": 0,
-      "spe": 0,
-      "atk": 0,
-      "def": 0,
-      "spa": 0,
-      "spd": 0
-    }
-  },
-  {
-    "id": "student",
-    "name": "E2(nin)",
-    "xp": 1100,
-    "nature": "nin",
-    "retreat": 3,
-    "types": [],
-    "abilities": [],
-    "items": [],
-    "moves": [
-      {
-        "id": "dirt",
-        "isSelected": true
-      }
-    ],
-    "mega": {
-      "moves": [],
-      "suffix": "mega"
-    },
-    "stats": {},
-    "token_used": {
-      "hp": 0,
-      "spe": 0,
-      "atk": 0,
-      "def": 0,
-      "spa": 0,
-      "spd": 0
-    }
-  }
-], [], "multiple")
+// startBattle([
+// {
+//   "nature": "none",
+//   "id": "rookie",
+//   "name": "Leader",
+//   "xp": 1100,
+//   "retreat": 4,
+//   "types": [
+//     "Normal"
+//   ],
+//   "abilities": [],
+//   "items": [],
+//   "moves": [
+//     {
+//       "id": "kunai",
+//       "isSelected": true,
+//       "isDefault": true
+//     },
+//     {
+//       "id": "ninjastar",
+//       "isSelected": true,
+//       "isDefault": true
+//     },
+//     {
+//       "id": "poisongas",
+//       "isSelected": true,
+//       "isDefault": true
+//     },
+//     {
+//       "id": "fakeout",
+//       "isSelected": true,
+//       "isDefault": true
+//     },
+//     {
+//       "id": "echoedvoice",
+//       "isSelected": true,
+//       "isDefault": true
+//     },
+//     {
+//       "id": "faketears"
+//     }
+//   ],
+//   "mega": {
+//     "moves": [],
+//     "suffix": "mega"
+//   },
+//   "stats": {},
+//   "token_used": {
+//     "hp": 0,
+//     "spe": 0,
+//     "atk": 0,
+//     "def": 0,
+//     "spa": 0,
+//     "spd": 0
+//   }
+// },  {
+//     "id": "student",
+//     "name": "E1(tai)",
+//     "xp": 1100,
+//     "nature": "tai",
+//     "retreat": 3,
+//     "types": [],
+//     "abilities": [],
+//     "items": [],
+//     "moves": [
+//       {
+//         "id": "poisonfang",
+//         "isSelected": true
+//       }
+//     ],
+//     "mega": {
+//       "moves": [],
+//       "suffix": "mega"
+//     },
+//     "stats": {},
+//     "token_used": {
+//       "hp": 0,
+//       "spe": 0,
+//       "atk": 0,
+//       "def": 0,
+//       "spa": 0,
+//       "spd": 0
+//     }
+//   },
+//   {
+//     "id": "student",
+//     "name": "E2(nin)",
+//     "xp": 1100,
+//     "nature": "nin",
+//     "retreat": 3,
+//     "types": [],
+//     "abilities": [],
+//     "items": [],
+//     "moves": [
+//       {
+//         "id": "dirt",
+//         "isSelected": true
+//       }
+//     ],
+//     "mega": {
+//       "moves": [],
+//       "suffix": "mega"
+//     },
+//     "stats": {},
+//     "token_used": {
+//       "hp": 0,
+//       "spe": 0,
+//       "atk": 0,
+//       "def": 0,
+//       "spa": 0,
+//       "spd": 0
+//     }
+//   }
+// ], [], "multiple")
