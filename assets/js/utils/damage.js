f7b125a4d8da3e1be6d004837d1b4b30767c097b
Hasan Ashab <samertask@gmail.com>
2025-09-02 19:10:34 +0600

    backup

--- assets/js/utils/damage.js
+++ assets/js/utils/damage.js
@@ -72,7 +72,7 @@ export class Damage {
 
         bp *= eventModifier * gradeModifier
         this.move._bp = bp
-        // console.log(this.move.id, ' BP: ', bp)
+        console.debug(this.move.id, ' BP: ', bp)
 
         const stab = this.attacker.isTypeOf(this.move.type) ? Damage.STAB_MODIFIER : 1
         const isSpecial = this.move.category === "Special";
