8a5475afd59950f79373b6e1d26bf5d194750a52
Hasan Ashab <samertask@gmail.com>
2025-09-08 10:23:10 +0600

    backup

--- assets/js/index.js
+++ assets/js/index.js
@@ -254,242 +254,244 @@ document.body.onload = loadAll;
 
 import { startBattle, startUserBattle } from "./utils/dom.js";
 
-startBattle([
-  {
-    "id": "rookie",
-    "name": "Co venom",
-    "xp": 2400,
-    "nature": "none",
-    "retreat": 4.5,
-    "types": [
-      "Poison"
-    ],
-    "abilities": [],
-    "items": [],
-    "moves": [
-      {
-        "id": "poisonsting",
-        "isSelected": true
-      },
-      {
-        "id": "acidspray",
-        "isSelected": true
-      },
-      {
-        "id": "takeweapon",
-        "isSelected": true
-      },
-      {
-        "id": "poisonfang",
-        "isSelected": true
-      },
-      {
-        "id": "chakrablade",
-        "isSelected": true
-      },
-      {
-        "id": "ninjastar",
-        "isSelected": true
-      },
-      {
-        "id": "clearsmog",
-        "isSelected": true
-      }
-    ],
-    "mega": {
-      "moves": [],
-      "suffix": "mega"
-    },
-    "stats": {},
-    "token_used": {
-      "hp": 0,
-      "spe": 0,
-      "atk": 0,
-      "def": 0,
-      "spa": 0,
-      "spd": 0
-    }
-  },
-  {
-    "id": "rookie",
-    "name": "E1",
-    "xp": 900,
-    "nature": "none",
-    "retreat": 3,
-    "types": [],
-    "abilities": [],
-    "items": [],
-    "moves": [
-      {
-        "id": "acid",
-        "isSelected": true
-      }
-    ],
-    "mega": {
-      "moves": [],
-      "suffix": "mega"
-    },
-    "stats": {},
-    "token_used": {
-      "hp": 0,
-      "spe": 0,
-      "atk": 0,
-      "def": 0,
-      "spa": 0,
-      "spd": 0
-    }
-  },
-    {
-    "id": "rookie",
-    "name": "E2",
-    "xp": 900,
-    "nature": "none",
-    "retreat": 3,
-    "types": [],
-    "abilities": [],
-    "items": [],
-    "moves": [
-      {
-        "id": "acid",
-        "isSelected": true
-      }
-    ],
-    "mega": {
-      "moves": [],
-      "suffix": "mega"
-    },
-    "stats": {},
-    "token_used": {
-      "hp": 0,
-      "spe": 0,
-      "atk": 0,
-      "def": 0,
-      "spa": 0,
-      "spd": 0
-    }
-  },
-    {
-    "id": "rookie",
-    "name": "E3",
-    "xp": 900,
-    "nature": "none",
-    "retreat": 3,
-    "types": [],
-    "abilities": [],
-    "items": [],
-    "moves": [
-      {
-        "id": "acid",
-        "isSelected": true
-      }
-    ],
-    "mega": {
-      "moves": [],
-      "suffix": "mega"
-    },
-    "stats": {},
-    "token_used": {
-      "hp": 0,
-      "spe": 0,
-      "atk": 0,
-      "def": 0,
-      "spa": 0,
-      "spd": 0
-    }
-  },
-    {
-    "id": "rookie",
-    "name": "E4",
-    "xp": 900,
-    "nature": "none",
-    "retreat": 3,
-    "types": [],
-    "abilities": [],
-    "items": [],
-    "moves": [
-      {
-        "id": "acid",
-        "isSelected": true
-      }
-    ],
-    "mega": {
-      "moves": [],
-      "suffix": "mega"
-    },
-    "stats": {},
-    "token_used": {
-      "hp": 0,
-      "spe": 0,
-      "atk": 0,
-      "def": 0,
-      "spa": 0,
-      "spd": 0
-    }
-  },
-    {
-    "id": "rookie",
-    "name": "E5",
-    "xp": 900,
-    "nature": "none",
-    "retreat": 3,
-    "types": [],
-    "abilities": [],
-    "items": [],
-    "moves": [
-      {
-        "id": "acid",
-        "isSelected": true
-      }
-    ],
-    "mega": {
-      "moves": [],
-      "suffix": "mega"
-    },
-    "stats": {},
-    "token_used": {
-      "hp": 0,
-      "spe": 0,
-      "atk": 0,
-      "def": 0,
-      "spa": 0,
-      "spd": 0
-    }
-  },
-    {
-    "id": "rookie",
-    "name": "E6",
-    "xp": 900,
-    "nature": "none",
-    "retreat": 3,
-    "types": [],
-    "abilities": [],
-    "items": [],
-    "moves": [
-      {
-        "id": "acid",
-        "isSelected": true
-      }
-    ],
-    "mega": {
-      "moves": [],
-      "suffix": "mega"
-    },
-    "stats": {},
-    "token_used": {
-      "hp": 0,
-      "spe": 0,
-      "atk": 0,
-      "def": 0,
-      "spa": 0,
-      "spd": 0
-    }
-  }
+
+
+// startBattle([
+//   {
+//     "id": "rookie",
+//     "name": "Co venom",
+//     "xp": 2400,
+//     "nature": "none",
+//     "retreat": 4.5,
+//     "types": [
+//       "Poison"
+//     ],
+//     "abilities": [],
+//     "items": [],
+//     "moves": [
+//       {
+//         "id": "poisonsting",
+//         "isSelected": true
+//       },
+//       {
+//         "id": "acidspray",
+//         "isSelected": true
+//       },
+//       {
+//         "id": "takeweapon",
+//         "isSelected": true
+//       },
+//       {
+//         "id": "poisonfang",
+//         "isSelected": true
+//       },
+//       {
+//         "id": "chakrablade",
+//         "isSelected": true
+//       },
+//       {
+//         "id": "ninjastar",
+//         "isSelected": true
+//       },
+//       {
+//         "id": "clearsmog",
+//         "isSelected": true
+//       }
+//     ],
+//     "mega": {
+//       "moves": [],
+//       "suffix": "mega"
+//     },
+//     "stats": {},
+//     "token_used": {
+//       "hp": 0,
+//       "spe": 0,
+//       "atk": 0,
+//       "def": 0,
+//       "spa": 0,
+//       "spd": 0
+//     }
+//   },
+//   {
+//     "id": "rookie",
+//     "name": "E1",
+//     "xp": 900,
+//     "nature": "none",
+//     "retreat": 3,
+//     "types": [],
+//     "abilities": [],
+//     "items": [],
+//     "moves": [
+//       {
+//         "id": "acid",
+//         "isSelected": true
+//       }
+//     ],
+//     "mega": {
+//       "moves": [],
+//       "suffix": "mega"
+//     },
+//     "stats": {},
+//     "token_used": {
+//       "hp": 0,
+//       "spe": 0,
+//       "atk": 0,
+//       "def": 0,
+//       "spa": 0,
+//       "spd": 0
+//     }
+//   },
+//     {
+//     "id": "rookie",
+//     "name": "E2",
+//     "xp": 900,
+//     "nature": "none",
+//     "retreat": 3,
+//     "types": [],
+//     "abilities": [],
+//     "items": [],
+//     "moves": [
+//       {
+//         "id": "acid",
+//         "isSelected": true
+//       }
+//     ],
+//     "mega": {
+//       "moves": [],
+//       "suffix": "mega"
+//     },
+//     "stats": {},
+//     "token_used": {
+//       "hp": 0,
+//       "spe": 0,
+//       "atk": 0,
+//       "def": 0,
+//       "spa": 0,
+//       "spd": 0
+//     }
+//   },
+//     {
+//     "id": "rookie",
+//     "name": "E3",
+//     "xp": 900,
+//     "nature": "none",
+//     "retreat": 3,
+//     "types": [],
+//     "abilities": [],
+//     "items": [],
+//     "moves": [
+//       {
+//         "id": "acid",
+//         "isSelected": true
+//       }
+//     ],
+//     "mega": {
+//       "moves": [],
+//       "suffix": "mega"
+//     },
+//     "stats": {},
+//     "token_used": {
+//       "hp": 0,
+//       "spe": 0,
+//       "atk": 0,
+//       "def": 0,
+//       "spa": 0,
+//       "spd": 0
+//     }
+//   },
+//     {
+//     "id": "rookie",
+//     "name": "E4",
+//     "xp": 900,
+//     "nature": "none",
+//     "retreat": 3,
+//     "types": [],
+//     "abilities": [],
+//     "items": [],
+//     "moves": [
+//       {
+//         "id": "acid",
+//         "isSelected": true
+//       }
+//     ],
+//     "mega": {
+//       "moves": [],
+//       "suffix": "mega"
+//     },
+//     "stats": {},
+//     "token_used": {
+//       "hp": 0,
+//       "spe": 0,
+//       "atk": 0,
+//       "def": 0,
+//       "spa": 0,
+//       "spd": 0
+//     }
+//   },
+//     {
+//     "id": "rookie",
+//     "name": "E5",
+//     "xp": 900,
+//     "nature": "none",
+//     "retreat": 3,
+//     "types": [],
+//     "abilities": [],
+//     "items": [],
+//     "moves": [
+//       {
+//         "id": "acid",
+//         "isSelected": true
+//       }
+//     ],
+//     "mega": {
+//       "moves": [],
+//       "suffix": "mega"
+//     },
+//     "stats": {},
+//     "token_used": {
+//       "hp": 0,
+//       "spe": 0,
+//       "atk": 0,
+//       "def": 0,
+//       "spa": 0,
+//       "spd": 0
+//     }
+//   },
+//     {
+//     "id": "rookie",
+//     "name": "E6",
+//     "xp": 900,
+//     "nature": "none",
+//     "retreat": 3,
+//     "types": [],
+//     "abilities": [],
+//     "items": [],
+//     "moves": [
+//       {
+//         "id": "acid",
+//         "isSelected": true
+//       }
+//     ],
+//     "mega": {
+//       "moves": [],
+//       "suffix": "mega"
+//     },
+//     "stats": {},
+//     "token_used": {
+//       "hp": 0,
+//       "spe": 0,
+//       "atk": 0,
+//       "def": 0,
+//       "spa": 0,
+//       "spd": 0
+//     }
+//   }
 
 
 
 
 
-], [], "multiple")
+// ], [], "multiple")
 // startBattle([
 //     {
 //   "nature": "none",
