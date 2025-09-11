7f07fa8c69fda5182d09b8e9039304b4cd23f5e3
Hasan Ashab <samertask@gmail.com>
2025-09-11 13:17:16 +0600

    backup

--- data/extras/moves.js
+++ data/extras/moves.js
@@ -1254,9 +1254,19 @@ katana: {
       type: "Poison",
       contestType: "Tough" 
     },
-    // auragloves: {
-
-    // }
+    auragloves: {
+      num: 100048,
+      accuracy: 100,
+      basePower: 10,
+      category: "Physical",
+      name: "Aura Gloves",
+      pp: 5 * 3,
+      priority: 0,
+      flags: { protect: 1, mirror: 1, metronome: 1, combo:1,contact:1 },
+      target: "normal",
+      type: "Fighting",
+      contestType: "Tough" 
+     },
   grassfield: FieldAddingMove("Grass", 2, "Grass Field"),
   removegrassfield: FieldRemovingMove("Grass", "Remove Grass Field")
 }
