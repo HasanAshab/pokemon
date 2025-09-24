2585774c845e3199fa2c7b125f60c581aead242e
Hasan Ashab <samertask@gmail.com>
2025-09-24 16:39:16 +0600

    backup

--- data/extras/moves.js
+++ data/extras/moves.js
@@ -1561,14 +1561,14 @@ katana: {
       target: "normal",
       type: "Normal",
       onHit(pokemon, opponent) {
-        const per = 0.15
+        const per = 0.30
         const stats = {
           atk: pokemon.stats.atk * per,
           spa: pokemon.stats.spa * per,
-        }
+        }        
         opponent.tokens = sumObj(opponent.tokens, stats)
         
-        opponent.state.once('scene-end', () => {
+        opponent.state.once('used-move', () => {
           opponent.tokens = sumObj(opponent.tokens, modObj(stats, -1))
         })
       }
