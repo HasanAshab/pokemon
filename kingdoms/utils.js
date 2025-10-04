0d9562f0439011c11ec4bcbdd12504533b0eb0fa
Hasan Ashab <samertask@gmail.com>
2025-10-04 18:00:33 +0600

    backup

--- kingdoms/utils.js
+++ kingdoms/utils.js
@@ -13,21 +13,23 @@ export function calculateBirthCount(population) {
   return Math.floor(annualBirths / 12);
 }
 
-export function calculateAgeDeathCount(population) {
+export function getPopulationGrowth(kingdom){
+  const population = getPopulation(kingdom)
+  return calculateBirthCount(population) - getTotalDeathCount(kingdom)
+}
+
+export function getTotalDeathCount(kingdom){
+  return getDiedForAge(kingdom)
+    + getDiedForHospital(kingdom)
+    + getDiedForSecurity(kingdom)
+}
+
+export function getDiedForAge(kingdom) {
+  const population = getPopulation(kingdom);
   const deathRatePer1000 = 10; // realistic average
   const annualDeaths = (population * deathRatePer1000) / 1000;
   return annualDeaths
   return Math.floor(annualDeaths / 12);
-}
-
-export function getPopulationGrowth(kingdom){
-  const population = getPopulation(kingdom)
-  const sysGrowth = calculateBirthCount(population) - calculateAgeDeathCount(population)
-  return (
-    sysGrowth
-    - getDiedForHospital(kingdom)
-    - getDiedForSecurity(kingdom)
-  )
 }
 
 export function getDiedForHospital(kingdom) {
@@ -49,12 +51,44 @@ export function getDiedForHospital(kingdom) {
 }
 
 
-export function getDiedForSecurity(kingdom){
-  return 10
+export function getDiedForSecurity(kingdom) {
+  const securityRate = getTotalSecurityRate(kingdom)
+  const deathRate = 1 - (securityRate / 100)
+  console.log(securityRate, deathRate);
+  const death = getPopulation(kingdom) * deathRate
+  console.log(death);
+  
+  return death
 }
 
-export function getSecurityRate(kingdom,forceType){
-  return 20
+export function getTotalSecurityRate(kingdom){
+  return Math.min(
+      getSecurityRate(kingdom, "polices")
+    + getSecurityRate(kingdom, "soldiers")
+  , 100)
+}
+
+export function getSecurityRate(kingdom, forceType){
+  const target = 92740.8;
+  const might = (getMight(kingdom, forceType, "day")
+    + getMight(kingdom, forceType, "night")) / 2
+
+  const closeness = (might / target) * 100;
+  return Number(closeness.toFixed(2))
+}
+
+export function getMight(kingdom, forceType, shift) {
+  const soldiers = kingdom.barrack[forceType]?.[shift]
+  if (!soldiers) return 0
+  const stack = new SoldierStack(soldiers.map(s => [s.image, s.quantity]))
+  if (stack.cp() === 0) return 0
+
+
+  const forceTypeMod = forceType === "soldiers" ? 0.08 : 1
+  const countMod = (stack.cp() / stack.count()) * 0.006
+  
+  const might = stack.cp() * forceTypeMod * countMod
+  return might
 }
 
 export function calculateLandPrice(
