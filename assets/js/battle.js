ac1148193e115022de8e71241b3e6f10763869fe
Hasan Ashab <samertask@gmail.com>
2025-10-11 16:30:24 +0600

    backup

--- assets/js/battle.js
+++ assets/js/battle.js
@@ -1009,14 +1009,14 @@ function setTotalHealth(className, hp, playerTag) {
   healthProgressBar.setAttribute("data-current-hp", hp)
   healthProgressBar.querySelector(".inner").style.width = '100%'
   healthProgressBar.querySelector(".current-hp").textContent = hp
-  healthProgressBar.querySelector(".total-hp").textContent = hp.toFixed(0)
+  healthProgressBar.querySelector(".total-hp").textContent = parseInt(hp)
 }
 
 function setCurrentHealth(className, hp, playerTag) {
   const pokemon = pokemonMap[playerTag]
   const healthProgressBar = document.querySelector(`.${playerTag}-controle-cont .${className}.progress-bar`)
   healthProgressBar.setAttribute("data-current-hp", hp)
-  healthProgressBar.querySelector(".current-hp").textContent = hp.toFixed(0)
+  healthProgressBar.querySelector(".current-hp").textContent = parseInt(hp)
   // if (className === "health")
   //   healthProgressBar.querySelector(".current-hp").textContent += `(${Math.round((hp * 100) / pokemon.maxhp)}%)`
   const progress = className === "health"
