b218846fc48c26f4c693ae217aaf37dd10fbe342
Hasan Ashab <samertask@gmail.com>
2025-10-18 09:19:26 +0600

    backup

---
+++ gyms/First/5-fire.js
@@ -0,0 +1,45 @@
+startBattle([
+  {
+    "id": "slugma",
+    "name": "E1",
+    "xp": 1000,
+    "nature": "none",
+    "retreat": 3.5,
+    "types": [],
+    "abilities": [],
+    "items": ["gen-food-low"],
+    "moves": [
+      {
+        "id": "smog",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "rockthrow",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "furysmoke",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "ember",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "tackle",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  }
+], [], "multiple")
