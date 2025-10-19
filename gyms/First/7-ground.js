fcfd880abbce5114cf14562b1da7f38a04e4d258
Hasan Ashab <samertask@gmail.com>
2025-10-19 08:09:23 +0600

    backup

---
+++ gyms/First/7-ground.js
@@ -0,0 +1,92 @@
+startBattle([
+  {
+    "id": "diglettalola",
+    "name": "Ea1",
+    "xp": 1400,
+    "nature": "none",
+    "retreat": 3.5,
+    "types": [],
+    "abilities": [],
+    "items": [
+      "gen-food-low"
+    ],
+    "moves": [
+      {
+        "id": "bulletpunch",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "sandattack",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "metalsound",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "astonish",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "mudslap",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  },
+  {
+    "id": "sandshrew",
+    "name": "E2",
+    "xp": 900,
+    "nature": "none",
+    "retreat": 3.5,
+    "types": [],
+    "abilities": [],
+    "items": [
+      "gen-food-low"
+    ],
+    "moves": [
+      {
+        "id": "scratch",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "sandattack",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "poisonsting",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "rapidspin",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "swift",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  }
+], ["Ground", "Steel"], "single")
