d1e995eb36b507d94e186696c99f7ff503d6c15f
Hasan Ashab <samertask@gmail.com>
2025-10-20 08:14:29 +0600

    backup

--- assets/js/index.js
+++ assets/js/index.js
@@ -253,6 +253,57 @@ document.body.onload = loadAll;
 
 import { startBattle, startUserBattle } from "./utils/dom.js";
 
+//last gym
+
+/*
+startBattle([
+  {
+    "id": "slugma",
+    "name": "E1",
+    "xp": 1200,
+    "nature": "none",
+    "retreat": 4,
+    "types": [],
+    "abilities": [],
+    "items": ["gen-food-low"],
+    "moves": [
+      {
+        "id": "smog",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "rockthrow",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "furysmoke",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "ember",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "tackle",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  }
+], ["Fire"], "single")
+
+
+*/
 
 
 
@@ -267,13 +318,6 @@ import { startBattle, startUserBattle } from "./utils/dom.js";
 
 
 
-
-
-
-
-
-
-// graund gym
 
 
 
