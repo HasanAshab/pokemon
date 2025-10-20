6379a1b555e25347db247d85e609c94ce6d333a4
Hasan Ashab <samertask@gmail.com>
2025-10-20 14:38:30 +0600

    backup

--- assets/js/index.js
+++ assets/js/index.js
@@ -255,55 +255,7 @@ import { startBattle, startUserBattle } from "./utils/dom.js";
 
 //last gym
 
-/*
-startBattle([
-  {
-    "id": "slugma",
-    "name": "E1",
-    "xp": 1200,
-    "nature": "none",
-    "retreat": 4,
-    "types": [],
-    "abilities": [],
-    "items": ["gen-food-low"],
-    "moves": [
-      {
-        "id": "smog",
-        "grade": "0",
-        "isSelected": true
-      },
-      {
-        "id": "rockthrow",
-        "grade": "0",
-        "isSelected": true
-      },
-      {
-        "id": "furysmoke",
-        "grade": "0",
-        "isSelected": true
-      },
-      {
-        "id": "ember",
-        "grade": "0",
-        "isSelected": true
-      },
-      {
-        "id": "tackle",
-        "grade": "0",
-        "isSelected": true
-      }
-    ],
-    "mega": {
-      "moves": [],
-      "suffix": "none"
-    },
-    "stats": {},
-    "token_used": {}
-  }
-], ["Fire"], "single")
 
-
-*/
 
 
 
