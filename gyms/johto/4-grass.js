1973cc69df77097a351b44c41e772c06fa206497
Hasan Ashab <samertask@gmail.com>
2025-10-22 09:01:29 +0600

    backup

---
+++ gyms/johto/4-grass.js
@@ -0,0 +1,45 @@
+startBattle([
+  {
+    "id": "oddish",
+    "name": "E1",
+    "xp": 1300,
+    "nature": "none",
+    "retreat": 3.5,
+    "types": [],
+    "abilities": [],
+    "items": [],
+    "moves": [
+      {
+        "id": "absorb",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "growth",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "sleeppowder",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "vinewhip",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "sunnyday",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  }
+], ["Grass"], "single")
---
+++ gyms/johto/5-poison.js
@@ -0,0 +1,92 @@
+startBattle([
+  {
+    "id": "nidorino",
+    "name": "m",
+    "xp": 1300,
+    "nature": "none",
+    "retreat": 3.5,
+    "types": [],
+    "abilities": [],
+    "items": [
+      "gen-food-low"
+    ],
+    "moves": [
+      {
+        "id": "hornattack",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "poisonsting",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "leer",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "peck",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "focusenergy",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "doublekick",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  },
+  {
+    "id": "nidorina",
+    "name": "f",
+    "xp": 1300,
+    "nature": "none",
+    "retreat": 3.25,
+    "types": [],
+    "abilities": [],
+    "items": [
+      "gen-food-low"
+    ],
+    "moves": [
+      {
+        "id": "scratch",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "poisongas",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "acid",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "substitute",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  }
+], [], "single")
