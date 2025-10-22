a77cf53ef6208a0e08abdec2f7621fd885d0e0c3
Hasan Ashab <samertask@gmail.com>
2025-10-22 06:44:30 +0600

    backup

---
+++ gyms/johto/3-electric.js
@@ -0,0 +1,52 @@
+startBattle([
+  {
+    "id": "voltorb",
+    "name": "volt",
+    "xp": 1200,
+    "nature": "none",
+    "retreat": 3.5,
+    "types": [],
+    "abilities": [],
+    "items": [
+      "gen-food-low"
+    ],
+    "moves": [
+      {
+        "id": "gyroball",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "charge",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "thundershock",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "eerieimpulse",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "rollout",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "protect",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  }
+], ["Electric"], "single")
