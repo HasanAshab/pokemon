3cf55b66efae7eabb08f85b843439a99a317ac2f
Hasan Ashab <samertask@gmail.com>
2025-10-23 07:32:56 +0600

    backup

---
+++ gyms/2-johto/league/3.js
@@ -0,0 +1,132 @@
+startBattle([
+  {
+    "id": "poliwhirl",
+    "name": "poli",
+    "xp": 1300,
+    "nature": "none",
+    "retreat": 3.5,
+    "types": [],
+    "abilities": [],
+    "items": [
+      "gen-food-low"
+    ],
+    "moves": [
+      {
+        "id": "pound",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "bubble",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "hypnosis",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "mudshot",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "poweruppunch",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  },
+  {
+    "id": "bulbasaur",
+    "name": "bulba",
+    "xp": 1100,
+    "nature": "none",
+    "retreat": 3.5,
+    "types": [],
+    "abilities": [],
+    "items": [
+      "gen-food-low"
+    ],
+    "moves": [
+      {
+        "id": "growl",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "vinewhip",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "leechseed",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "razorleaf",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "poisonpowder",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  },
+  {
+    "id": "honedge",
+    "name": "hon",
+    "xp": 1300,
+    "nature": "none",
+    "retreat": 3.5,
+    "types": [],
+    "abilities": [],
+    "items": [
+      "gen-food-low"
+    ],
+    "moves": [
+      {
+        "id": "swordsdance",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "furycutter",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "shadowsneak",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "metalsound",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  }
+], [], "single")
