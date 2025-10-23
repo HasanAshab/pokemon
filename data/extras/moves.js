874f12c30405457b35a87bbe56336776c5cd6e2a
Hasan Ashab <samertask@gmail.com>
2025-10-23 12:51:24 +0600

    backup

--- data/extras/moves.js
+++ data/extras/moves.js
@@ -1770,5 +1770,65 @@ katana: {
       type: "Grass",
       contestType: "Cool"
     },
+
+        flamecharge: {
+      num: 488,
+      accuracy: 100,
+      basePower: 40,
+      category: "Physical",
+      name: "Flame Charge",
+      pp: 20,
+      priority: 0,
+      flags: { contact: 1, protect: 1, mirror: 1, metronome: 1 },
+      secondary: {
+        chance: 100,
+        self: {
+          boosts: {
+            spe: 1
+          }
+        }
+      },
+      target: "normal",
+      type: "Fire",
+      contestType: "Cool"
+    },
+        icywind: {
+      num: 196,
+      accuracy: 95,
+      basePower: 55,
+      category: "Special",
+      name: "Icy Wind",
+      pp: 15,
+      priority: 0,
+      flags: { protect: 1, mirror: 1, metronome: 1, wind: 1 },
+      secondary: {
+        chance: 25,
+        boosts: {
+          spe: -1
+        }
+      },
+      target: "allAdjacentFoes",
+      type: "Ice",
+      contestType: "Beautiful"
+    },
+        electroweb: {
+      num: 527,
+      accuracy: 95,
+      basePower: 55,
+      category: "Special",
+      name: "Electroweb",
+      pp: 15,
+      priority: 0,
+      flags: { protect: 1, mirror: 1, metronome: 1 },
+      secondary: {
+        chance: 30,
+        boosts: {
+          spe: -1
+        }
+      },
+      target: "allAdjacentFoes",
+      type: "Electric",
+      contestType: "Beautiful"
+    },
 }
 
