d2c15743cedba00dff622c486ee81601c614aca5
Hasan Ashab <samertask@gmail.com>
2025-10-23 06:30:14 +0600

    backup

--- data/default/moves.js
+++ data/default/moves.js
@@ -5299,7 +5299,7 @@ export default {
       priority: 0,
       flags: { protect: 1, mirror: 1, metronome: 1 },
       secondary: {
-        chance: 100,
+        chance: 30,
         boosts: {
           spe: -1
         }
