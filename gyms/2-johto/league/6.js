313863fb46e02923a786a61eb809f9540caa955a
Hasan Ashab <samertask@gmail.com>
2025-10-25 07:59:58 +0600

    backup

---
+++ gyms/2-johto/league/6.js
@@ -0,0 +1,129 @@
+[
+
+    {
+    "id": "honedge",
+    "name": "hone",
+    "xp": 1300,
+    "nature": "none",
+    "retreat": 3.5,
+    "types": [],
+    "abilities": [],
+    "items": [
+      "gen-food-low"
+    ],
+    "moves": [
+      {
+        "id": "swordsdance",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "cut",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "shadowsneak",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "metalsound",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "brutalswing",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  },
+  {
+    "id": "kirlia",
+    "name": "kirlia",
+    "xp": 1300,
+    "nature": "none",
+    "retreat": 3.75,
+    "types": [],
+    "abilities": [],
+    "items": [
+      "gen-food-low"
+    ],
+    "moves": [
+      {
+        "id": "confusion",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "doubleteam",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "magicalleaf",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "disarmingvoice",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "raindance",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  },
+  {
+    "id": "fletchling",
+    "name": "fle",
+    "xp": 1000,
+    "nature": "none",
+    "retreat": 3.5,
+    "types": [],
+    "abilities": [],
+    "items": [
+      "gen-food-low"
+    ],
+    "moves": [
+      {
+        "id": "peck",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "quickattack",
+        "grade": "0",
+        "isSelected": true
+      },
+      {
+        "id": "flamecharge",
+        "grade": "0",
+        "isSelected": true
+      }
+    ],
+    "mega": {
+      "moves": [],
+      "suffix": "none"
+    },
+    "stats": {},
+    "token_used": {}
+  }
+
+]
