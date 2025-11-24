a09382398e7c05609414e710232455db7c0cf789
Hasan Ashab <samertask@gmail.com>
2025-11-24 11:30:41 +0600

    backup

--- assets/js/utils/battle.js
+++ assets/js/utils/battle.js
@@ -4,6 +4,7 @@ import { EffectManager } from "./effects.js"
 import { makeField } from "./fields.js"
 import { Hit } from "./damage.js"
 import { fixFloat, weightedRandom, sumObj, modObj, calcLevelStat } from "./helpers.js"
+import { WeatherManager } from "./weathers.js";
 
 
 class BaseBattle extends EventEmitter {
@@ -32,6 +33,7 @@ class BaseBattle extends EventEmitter {
         this.pokemon2 = this.team2[0]
         this._all = [...this.team1, ...this.team2]
         this.fields = fieldTypes.map(f => makeField(this, f))
+        this.weathers = new WeatherManager(this)
 
         this._all.forEach(p => {
             if (!p.state) {
@@ -949,6 +951,9 @@ class BattleState extends EventEmitter {
                     on: "target",
                     pre: true,
                 })
+
+                move.weather && this.battle.weathers.set(move, move.weather)
+
             }
         })
         
