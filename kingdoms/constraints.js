b6f59c848ff57fd51aed443a9940d7b5133a2f9b
Hasan Ashab <samertask@gmail.com>
2025-11-26 11:27:25 +0600

    backup

--- kingdoms/constraints.js
+++ kingdoms/constraints.js
@@ -10,4 +10,20 @@ export const FOOD_BUDGET = {
   "high-2": 8000,
   "high-3": 10000,
   "bulk": 15000
-} 
+}
+
+export const DISASTERS = {
+  "Earthquake": "Sudden shaking of the ground caused by the movement of tectonic plates.",
+  "Tsunami": "Giant waves usually caused by an underwater earthquake, volcanic eruption, or landslide.",
+  "Volcanic Eruption": "The release of magma, ash, and gases from a volcano.",
+  "Landslide": "Downslope movement of rock, debris, earth, or snow.",
+  "Cyclone": "Rotating storm systems forming over warm ocean waters, bringing strong winds, heavy rain, and storm surges.",
+  "Tornado": "Violent, rotating columns of air extending from a thunderstorm to the ground.",
+  "Flood": "Overflow of a large amount of water beyond its normal limits, especially over what is normally dry land.",
+  "Drought": "Prolonged periods of abnormally low rainfall, leading to water shortage.",
+  "Severe Storm": "Includes major events like blizzards, hailstorms, and extreme thunderstorms.",
+  "Heat Wave": "Extremely hot temperatures in a short period of time",
+  "Cold Wave": "Extremely cold temperatures in a short period of time",
+  "Wildfire": "Uncontrolled fires in a natural area, often accelerated by drought and heat.",
+  "Soil Erosion": "The loss of soil and rock in river area due to the action of water, wind, or other forces.",
+};
