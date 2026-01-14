bdc57b965e87bdc571abc5c24c65d67ea1b10248
HasanAshab <hasanashab.18205@gmail.com>
2026-01-14 08:01:36 +0600

    backup

--- data/beasts.js
+++ data/beasts.js
@@ -3,7 +3,7 @@ export default {
     num: 1,
     name: "Laimonk",
     types: ["Fighting"],
-    baseStats: { hp: 60, atk: 85, def: 65, spa: 60, spd: 65, spe: 64 }, // total = 399
+    baseStats: { hp: 66, atk: 90, def: 70, spa: 65, spd: 70, spe: 38 },
     abilities: {},
     heightm: 1.2
   },
@@ -12,7 +12,7 @@ export default {
     num: 2,
     name: "Devil",
     types: ["Dark"],
-    baseStats:  { hp: 85, atk: 95, def: 85, spa: 120, spd: 85, spe: 60 }, // total = 530
+    baseStats: { hp: 90, atk: 100, def: 90, spa: 125, spd: 89, spe: 36 },
     abilities: {},
     heightm: 1.3
   },
@@ -21,7 +21,7 @@ export default {
     num: 3,
     name: "Drago",
     types: ["Dragon", "Flying"],
-    baseStats: { hp: 95, atk: 120, def: 95, spa: 120, spd: 98, spe: 50 }, // total = 678
+    baseStats: { hp: 99, atk: 124, def: 99, spa: 124, spd: 102, spe: 30 },
     abilities: {},
     heightm: 1.4
   },
@@ -30,7 +30,7 @@ export default {
     num: 4,
     name: "Gobking",
     types: ["Poison"],
-    baseStats: { hp: 130, atk: 145, def: 130, spa: 110, spd: 115, spe: 79 }, // total = 809
+    baseStats: { hp: 137, atk: 152, def: 136, spa: 116, spd: 121, spe: 47 },
     abilities: {},
     heightm: 1.5
   },
@@ -39,7 +39,7 @@ export default {
     num: 5,
     name: "Elef",
     types: ["Normal"],
-    baseStats: { hp: 170, atk: 165, def: 160, spa: 140, spd: 135, spe: 75 }, // total = 985
+    baseStats: { hp: 176, atk: 171, def: 166, spa: 146, spd: 141, spe: 45 },
     abilities: {},
     heightm: 1.8
   },
@@ -48,14 +48,7 @@ export default {
     num: 6,
     name: "Dino",
     types: ["Dragon", "Dark"],
-    baseStats:  {
-    hp: 200,
-    atk: 220,
-    def: 260,
-    spa: 180,
-    spd: 298,
-    spe: 20
-  }, // total = 1178
+    baseStats: { hp: 202, atk: 222, def: 262, spa: 181, spd: 299, spe: 12 },
     abilities: {},
     heightm: 2
   },
@@ -64,15 +57,16 @@ export default {
     num: 7,
     name: "Knight",
     types: ["Steel"],
-    baseStats: { hp: 240, atk: 260, def: 290, spa: 170, spd: 290, spe: 136 }, // total = 1386
+    baseStats: { hp: 251, atk: 271, def: 301, spa: 181, spd: 301, spe: 81 },
     abilities: {},
     heightm: 2.3
   },
+
   tails1: {
     num: 8,
     name: "tails1( shukaku )",
     types: ["Ground"],
-    baseStats: { hp: 362, atk: 265, def: 335, spa: 307, spd: 290, spe: 84 },
+    baseStats: { hp: 369, atk: 272, def: 342, spa: 314, spd: 296, spe: 50 },
     abilities: {},
     heightm: 15.0
   },
@@ -81,7 +75,7 @@ export default {
     num: 9,
     name: "tails2( matatabi )",
     types: ["Fire", "Dark"],
-    baseStats: { hp: 390, atk: 279, def: 335, spa: 335, spd: 307, spe: 167 },
+    baseStats: { hp: 404, atk: 293, def: 348, spa: 348, spd: 320, spe: 100 },
     abilities: {},
     heightm: 14.0
   },
@@ -90,7 +84,7 @@ export default {
     num: 10,
     name: "tails3( isobu )",
     types: ["Rock"],
-    baseStats: { hp: 488, atk: 390, def: 460, spa: 349, spd: 209, spe: 125 },
+    baseStats: { hp: 498, atk: 400, def: 470, spa: 359, spd: 219, spe: 75 },
     abilities: {},
     heightm: 12.0
   },
@@ -99,7 +93,7 @@ export default {
     num: 11,
     name: "tails4( songoku )",
     types: ["Normal", "Fire", "Fighting"],
-    baseStats: { hp: 558, atk: 446, def: 418, spa: 432, spd: 293, spe: 139 },
+    baseStats: { hp: 570, atk: 457, def: 429, spa: 443, spd: 304, spe: 83 },
     abilities: {},
     heightm: 13.0
   },
@@ -108,7 +102,7 @@ export default {
     num: 12,
     name: "tails5( kokuo )",
     types: ["Fairy", "Psychic"],
-    baseStats: { hp: 586, atk: 349, def: 390, spa: 558, spd: 349, spe: 209 },
+    baseStats: { hp: 603, atk: 366, def: 407, spa: 575, spd: 365, spe: 125 },
     abilities: {},
     heightm: 14.0
   },
@@ -117,7 +111,7 @@ export default {
     num: 13,
     name: "tails6( saiken )",
     types: ["Poison", "Bug"],
-    baseStats: { hp: 627, atk: 418, def: 488, spa: 558, spd: 349, spe: 209 },
+    baseStats: { hp: 644, atk: 435, def: 505, spa: 575, spd: 365, spe: 125 },
     abilities: {},
     heightm: 15.0
   },
@@ -126,7 +120,7 @@ export default {
     num: 14,
     name: "tails7( killerbee )",
     types: ["Bug", "Flying"],
-    baseStats: { hp: 669, atk: 488, def: 558, spa: 586, spd: 418, spe: 139 },
+    baseStats: { hp: 681, atk: 499, def: 569, spa: 597, spd: 429, spe: 83 },
     abilities: {},
     heightm: 16.0
   },
@@ -135,7 +129,7 @@ export default {
     num: 15,
     name: "tails8( gyuki )",
     types: ["Water", "Dark"],
-    baseStats: { hp: 697, atk: 558, def: 627, spa: 627, spd: 418, spe: 139 },
+    baseStats: { hp: 709, atk: 569, def: 638, spa: 638, spd: 429, spe: 83 },
     abilities: {},
     heightm: 17.0
   },
@@ -144,7 +138,7 @@ export default {
     num: 16,
     name: "tails9( kurama )",
     types: ["Normal"],
-    baseStats: { hp: 697, atk: 697, def: 488, spa: 697, spd: 488, spe: 209 },
+    baseStats: { hp: 714, atk: 714, def: 505, spa: 714, spd: 504, spe: 125 },
     abilities: {},
     heightm: 18.0
   },
@@ -153,7 +147,7 @@ export default {
     num: 17,
     name: "Black Tai",
     types: ["Fighting"],
-    baseStats: { hp: 56, atk: 84, def: 63, spa: 21, spd: 42, spe: 14 },
+    baseStats: { hp: 58, atk: 85, def: 64, spa: 22, spd: 43, spe: 8 },
     abilities: {},
     heightm: 1.2
   },
@@ -162,42 +156,35 @@ export default {
     num: 18,
     name: "Rock Tri",
     types: ["Fighting", "Rock"],
-    baseStats: { hp: 105, atk: 105, def: 119, spa: 28, spd: 98, spe: 28 },
+    baseStats: { hp: 108, atk: 108, def: 121, spa: 30, spd: 100, spe: 16 },
     abilities: {},
-    heightm: 1.4,
-    desc: `Has 30% to repeat user used move if make contact = true.
-    it has 3 hands so it can repeat 3 times`
+    heightm: 1.4
   },
 
   roaringhands: {
     num: 19,
     name: "Roaring Hands",
     types: ["Fighting", "Rock", "Dark"],
-    baseStats: { hp: 279, atk: 237, def: 209, spa: 70, spd: 209, spe: 70 },
+    baseStats: { hp: 285, atk: 243, def: 215, spa: 75, spd: 214, spe: 42 },
     abilities: {},
-    heightm: 1.6,
-    desc: `Has 30% to repeat user used move if make contact = true.
-    it has 6 hands so it can repeat 6 times`
+    heightm: 1.6
   },
 
   gigazardx: {
     num: 20,
     name: "Gigazard X",
     types: ["Fire", "Dragon"],
-    baseStats: { hp: 767, atk: 976, def: 767, spa: 418, spd: 558, spe: 279 },
+    baseStats: { hp: 790, atk: 999, def: 789, spa: 440, spd: 580, spe: 167 },
     abilities: {},
-    heightm: 4.5,
-    description: "A mutant four-armed titan. Its physical strength is unmatched, allowing it to crush mountains with its bare hands."
+    heightm: 4.5
   },
 
   gigazardy: {
     num: 21,
     name: "Gigazard Y",
     types: ["Fire", "Flying"],
-    baseStats: { hp: 488, atk: 418, def: 558, spa: 1046, spd: 767, spe: 418 },
+    baseStats: { hp: 522, atk: 452, def: 592, spa: 1079, spd: 800, spe: 250 },
     abilities: { 0: "Drought" },
-    heightm: 4.2,
-    description: "An evolved aerial mutant. It can incinerate entire landscapes with a single blast of special draconic energy."
+    heightm: 4.2
   }
-
 };
